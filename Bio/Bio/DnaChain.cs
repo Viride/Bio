4b9771b5ca3bc3080fcffad13ca2d73cd534f9b7
Viride <pawel.kaczmarek@ingonyama.pl>
2017-06-01 22:43:14 +0200

    added more information to result files

--- Bio/Bio/DnaChain.cs
+++ Bio/Bio/DnaChain.cs
@@ -165,7 +165,8 @@ namespace Bio
             System.IO.StreamWriter file = System.IO.File.CreateText(file_name);
             file.WriteLine("Working time: {0}", time);
             file.WriteLine("Repeats count: {0}", NmbOfRepeats);
-            file.WriteLine("Number of Oligonukleotyd#: {0}\n", StringOfOlig.Count());
+            file.WriteLine("Number of Oligonukleotyd#: {0}", StringOfOlig.Count());
+            file.WriteLine("Chain size: {0}\n", SequenceLength);
 
             file.Write("{0}", StringOfOlig[0].Ciag);
             for (int i = 1; i < StringOfOlig.Count(); i++)
